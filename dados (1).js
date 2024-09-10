8bb70250af92d1457a3898603c16dc6a196e018e
Priscilla - Foursys <59477854+priscilla4sys@users.noreply.github.com>
2024-09-09 23:53:01 -0300

    Add files via upload

---
+++ dados (1).js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo: "O povo judeu",
+        descricao: "O povo judeu, segundo a Bíblia Sagrada, tem suas origens em Abraão, considerado o pai da fé pelas três grandes religiões monoteístas: judaísmo, cristianismo e islamismo.",
+        link: "https://pt.wikipedia.org/wiki/Juda%C3%ADsmo#:~:text=Os%20judeus%20s%C3%A3o%20um%20grupo,2%25%20da%20popula%C3%A7%C3%A3o%20mundial%20total.",
+    tags: "povo judeu judaísmo hebreus israelitas religião judaica cultura judaica história judaica"
+},
+{
+    titulo: "As divisões sociopolíticas no tempo de Jesus e de Paulo",
+    descricao: "O período em que Jesus e Paulo viveram foi marcado por diversas divisões sociopolíticas, incluindo o domínio romano e as tensões entre os grupos judaicos.",
+    link : "https://formacao.cancaonova.com/biblia/historia/como-era-a-sociedade-no-tempo-de-jesus/",
+    tags: "Império Romano Província Romana da Judeia História Romana"
+},
+
+{
+    titulo: "As seitas judaicas",
+    descricao: "O judaísmo antigo era marcado por diversas seitas, cada uma com suas próprias crenças e práticas. As principais seitas eram os fariseus, saduceus e essênios.",
+    link : "https://opusdei.org/pt-br/article/quem-eram-os-fariseus-os-saduceus-os-essenios-e-os-zelotes/",
+tags: "Fariseus Saduceus Essênios"
+},
+];
+
